b946bc2874f6aafec8dac048ae5cdcdc1deff8f8
Saksham <sakshamloonker@gmail.com>
2026-08-26 12:55:31 +0530

    Add Move-Zeroes (two-pointers)

---
+++ leetcode/two-pointers/283-Move-Zeroes.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        non_zero_position = 0
+        for current_index, current_value in enumerate(nums):
+            if current_value != 0:
+                nums[non_zero_position], nums[current_index] = nums[current_index], nums[non_zero_position]
+                non_zero_position += 1
